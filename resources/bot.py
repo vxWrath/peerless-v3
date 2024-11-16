c0c46c2e3bd6c3511b023bdcf2265556c3561d7a
vxWrath <kylecain111@gmail.com>
2024-11-16 16:21:07 -0600

    Add pre-command handling

--- resources/bot.py
+++ resources/bot.py
@@ -5,9 +5,10 @@ import discord
 import os
 import sys
 
+from collections import defaultdict
 from discord.app_commands import Command, CommandTree
 from discord.ext import commands
-from typing import List
+from typing import List, Optional, Any, Set
 
 intents = discord.Intents.none()
 intents.guilds  = True
@@ -17,8 +18,11 @@ intents.members = True
 member_cache_flags = discord.MemberCacheFlags().none()
 member_cache_flags.joined = True
 
+from .models import BeforeInteraction
 from .database import Database
+from .namespace import Namespace
 from .redis import RedisClient
+from .utils import respond
 
 class Bot(commands.Bot):
     def __init__(self) -> None:
@@ -33,6 +37,8 @@ class Bot(commands.Bot):
 
         self.redis: RedisClient
         self.database: Database
+
+        self.guilds_erroring: defaultdict[int, datetime.datetime] = defaultdict(discord.utils.utcnow)
 
     async def setup_hook(self) -> None:
         await self.load_extensions()
@@ -53,7 +59,7 @@ class Bot(commands.Bot):
             except commands.ExtensionNotLoaded:
                 await self.load_extension(cog)
         
-        dont_load = []
+        dont_load: List[str] = []
         for dir_, _, files in os.walk('./commands'):
             for file in files:
                 if not file.endswith('.py') or file in dont_load:
@@ -78,4 +84,97 @@ class Bot(commands.Bot):
         return None
 
 class AppCommandTree(CommandTree[Bot]):
-    pass
+    def _from_interaction(self, interaction: discord.Interaction[Bot]) -> None:
+        async def wrapper():
+            try:
+                await self._call(interaction)
+            except discord.app_commands.AppCommandError as e:
+                await self._dispatch_error(interaction, e)
+
+        self.client.loop.create_task(wrapper(), name='CommandTree-invoker')
+
+    async def interaction_check(self, interaction: discord.Interaction[Bot]) -> bool:
+        if interaction.guild and interaction.guild.unavailable:
+            try:
+                await interaction.response.send_message(
+                    content = "<:fail:1136341671857102868>**| This server is unavailable. This is a discord issue.**", 
+                    ephemeral = True
+                )
+            except discord.HTTPException:
+                pass
+            
+            return False
+        
+        data: Namespace[str, Namespace[str, Any]] = Namespace(interaction.data) if interaction.data else Namespace() # type: ignore[arg-type]
+
+        if interaction.command:
+            before: BeforeInteraction = interaction.command.extras.get('before', BeforeInteraction())
+        else:
+            before = interaction.extras.get('before', BeforeInteraction())
+
+        if not before.is_modal_response and before.defer:
+            await interaction.response.defer(ephemeral=before.ephemerally, thinking=before.thinking)
+
+        tasks: Namespace[str, Optional[asyncio.Task[Any]]] = Namespace(chunk_guild=None, get_league=None)
+
+        if interaction.guild:
+            if not interaction.guild.chunked:
+                tasks.chunk_guild = self.client.loop.create_task(interaction.guild.chunk())
+            
+                if not before.is_modal_response and not before.defer:
+                    await interaction.response.defer(ephemeral=before.ephemerally, thinking=before.thinking)
+
+            if before.league_data or interaction.type == discord.InteractionType.application_command:
+                tasks.get_league = self.client.loop.create_task(
+                    self.client.database.produce_league(interaction.guild.id)
+                )
+
+        player_ids: List[int] = []
+        if before.player_data:
+            player_ids.append(interaction.user.id)
+
+            if data.has('resolved'):
+                for user_id, discord_user_data in (data.resolved.get('members') or data.resolved.get('users', {})).items():
+                    if interaction.user.id == int(user_id) or discord_user_data.get('bot', False) or discord_user_data.get('user', {}).get('bot', False):
+                        continue
+
+                    player_ids.append(int(user_id))
+
+        if any(tasks.values()) or player_ids:
+            try:
+                async with asyncio.timeout(15 if interaction.response.is_done() else 2):
+                    if tasks.get_league:
+                        league_task, _ = await asyncio.wait([tasks.get_league], timeout=None)
+                        interaction.extras['league'] = league_task.pop().result()
+                        
+                    if player_ids:
+                        results, _ = await asyncio.wait([
+                            self.client.loop.create_task(self.client.database.produce_player(x, interaction.extras['league'])) for x in player_ids
+                        ], timeout=None)
+
+                    if tasks.chunk_guild:
+                        await asyncio.wait([tasks.chunk_guild], timeout=None)
+
+                if player_ids:
+                    for player_task in results:
+                        player = player_task.result()
+                        interaction.extras['players'][player.id] = player
+
+            except asyncio.TimeoutError:
+                dead_keys = [x for x, y in self.client.guilds_erroring.items() if y < discord.utils.utcnow() - datetime.timedelta(minutes=5)]
+                for dead_key in dead_keys:
+                    self.client.guilds_erroring.pop(dead_key)
+
+                if interaction.guild and self.client.guilds_erroring[interaction.guild.id] < discord.utils.utcnow() - datetime.timedelta(seconds=20):
+                    await respond(interaction, content=(
+                        "### ❌ Possible reasons for this server having continuous errors when trying to run commands:\n"
+                        "- This server is being chunked *(Chunking is basically the bot loading up the server)*\n"
+                        "- Database issues\n"
+                        "- Discord issues\n"
+                        "- The bot simply needs a restart\n"
+                    ), ephemeral=True)
+                else:
+                    await respond(interaction, content="❌ **Please try again in a few seconds**", ephemeral=True)
+                return False
+
+        return True
--- resources/models/interactions.py
+++ resources/models/interactions.py
@@ -4,13 +4,13 @@ from .base import BaseModel
 from ..namespace import Namespace
 
 class BeforeInteraction(BaseModel):
-    defer: Optional[bool]=False
-    ephemerally: Optional[bool]=False
-    thinking: Optional[bool]=False
-    league_data: Optional[bool]=False
-    player_data: Optional[bool]=False
+    defer: bool = False
+    ephemerally: bool = False
+    thinking: bool = False
+    league_data: bool = False
+    player_data: bool = False
 
-    is_modal_response: Optional[bool]=False
+    is_modal_response: bool = False
 
 class BeforeView(BaseModel):
     all_components: Optional[BeforeInteraction] = None
